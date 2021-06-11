d65845a59f8f8ecac2e761cedeb83b91d38fb626
Josiel Faria <josielsifa@gmail.com>
2021-06-11 18:39:14 -0300

    Adicionando angular material e sidenav responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements AfterViewInit {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) { }
+
+  ngAfterViewInit(): void {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { CommonModule, registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartComponent } from './components/chart/chart.component';
 import { FormIncludesValuesComponent } from './components/form-includes-values/form-includes-values.component';
+import { MaterialAngularModule } from './material.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 registerLocaleData(localePt, 'pt');
@@ -21,6 +24,9 @@ registerLocaleData(localePt, 'pt');
     CommonModule,
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialAngularModule,
+    NgxMaterialTimepickerModule.setLocale('pt-BR'),
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' }
