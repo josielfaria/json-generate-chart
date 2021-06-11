b3e84bc95632c1a32e72cf36f81f57b853464e8d
Josiel Faria <josielsifa@gmail.com>
2021-06-11 11:32:46 -0300

    Primeiro commit para rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', component: HomePageComponent } // url not found
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
